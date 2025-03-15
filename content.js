666f98bace44296c781598519aa6ee5ed0d8d258
alk <kimura-aruku@users.noreply.github.com>
2025-03-15 14:04:44 +0900

    update_キャラ変更でも動くようになった。

--- content.js
+++ content.js
@@ -2,19 +2,13 @@
 
 window.onload = () => {
     const MY_CLASS = 'alk-element';
+    const MY_OVERLAY_ID = 'alk-overlay';
     // 画面にチェックボックスを追加し、UID+キャラクターごとに追加ステータスを記憶する
     // 上記はchrome.storage.localで実現する
     // 最初に画面を開いたとき、キャラを切り替えたときに各ドライバをクリックして画面表示+ステータスをキャッシュ
     // 自作チェックボックスの内容で計算
     // 自作チェックボックスが変更されたらキャッシュしたステータスで計算
     
-    // ポップアップの親
-    /** @type {HTMLElement | null} */
-    let roleDetailElement;
-
-    // ポップアップ監視オブジェクト
-    let roleDetailElementObserver;
-
     // ドライバ情報リスト
     /** @type {Array<{
      *  iconSource: string, 
@@ -30,7 +24,13 @@ window.onload = () => {
      *  }>} */
     const driverInfoList = [];
 
-    // サブステータスの数が足りないとき、空行でスコアの位置を揃える
+    // キャラ親要素
+    /** @type {HTMLElement | null} */
+    let characterInfoElement;
+
+    // キャラ情報の監視オブジェクト
+    let characterInfoElementObserver;
+
     
     // .property-info内のli内から、各ステータスのアイコンを取得し、キーで取得できるようにする
     /** @type {{ [key: string]: string }} */
@@ -106,11 +106,9 @@ window.onload = () => {
 
         // ドライバ情報のキャッシュ
         async function cacheDriverInfo(driverIndex){
-            console.log('キャッシュ開始');
             const popupContentElement = await waitForElement('.popup-content', null);
             const driverInfo = {};
             driverInfo.iconSource = popupContentElement.querySelector('img')?.getAttribute('src');
-            console.log('アイコンのパス設定');
             const nameAndLevelElement = popupContentElement.querySelectorAll('p');
             driverInfo.driverName = nameAndLevelElement[0].textContent.trim();
             driverInfo.driverRarityClassName = nameAndLevelElement[0].className;
@@ -122,7 +120,6 @@ window.onload = () => {
             const subPropNameAndValues = [];
             supPropElements.forEach(div => {
                 const spans = div.querySelectorAll('span');
-                // 直下に span が2つだけある場合
                 if (spans.length === 2) {
                     const subPropNameAndValue = {};
                     subPropNameAndValue.name = spans[0].textContent.trim();
@@ -131,7 +128,6 @@ window.onload = () => {
                 }
             });
             driverInfo.subPropNameAndValues = subPropNameAndValues;
-            console.log(driverInfo);
             driverInfoList[driverIndex] = driverInfo;
             popupContentElement.parentNode.querySelector('.close-icon').click();
         }
@@ -186,51 +182,20 @@ window.onload = () => {
         
         // 描画
         function draw(){
-        }
-    
-        // 非同期処理を分離
-        async function reDraw() {
-            draw();
-        }
-    
-        // 最初に実行
-        async function setup(){
-            // TODO:装備がない場合、タイムアウトになるので早めに装備なしを判断したい
-            //  ->セット効果がない + ステータスにプラスがない場合
-            const equipInfoElements = await waitForElements('.equip-info', 
-                (els) => Array.from(els).some(el => el.querySelector('.bg')),
-                undefined,
-                () => !!document.querySelector('.base-add-prop') &&
-                    !!document.querySelector('.empty-content'));
-            roleDetailElement = document.querySelector('.role-detail');
-            // ドライバを装備しているかどうか
-            const equipDriverList = Array.from(equipInfoElements)
-                .map(el => !!el.querySelector('.bg'));
-            // TODO:装備しているドライバがないなら、キャラ変更監視だけ行う
-            const isNothingEquipped = equipDriverList.every(value => value === false);
-            
-            // .equip-infoで要素を取得できたらボタンを押す->情報取得->閉じる の繰り返し
-            for (let i = 0; i < equipDriverList.length; i++) {
-                if (equipDriverList[i]) {
-                    equipInfoElements[i].click();
-                    await cacheDriverInfo(i);
-                } else {
-                    driverInfoList[i] = null;
-                }
-            }
-
-            console.log('遺物情報取得完了');
-            console.log(driverInfoList);
-            // 情報を全部取り終えたら画面を作る
+            // 念のため削除
+            document.querySelectorAll(`.${MY_CLASS}`)?.forEach(element => {
+                element.remove();
+            });
             // とりあえず親にする要素
             const parentElement = document.querySelector('.equipment-info');
             const mainFrameElement = document.createElement('div');
+            mainFrameElement.classList.add(MY_CLASS);
             let totalScores = 0;
-            equipDriverList.forEach((equipDriver, index) => {
+            driverInfoList.forEach((driverInfo, index) => {
                 const driverScoreElement = document.createElement('div');
                 driverScoreElement.style.height = '20px';
-                if (equipDriver) {
-                    const subPropNameAndValues = driverInfoList[index].subPropNameAndValues;
+                if (driverInfo) {
+                    const subPropNameAndValues = driverInfo.subPropNameAndValues;
                     let scores = 0
                     subPropNameAndValues.forEach(subProp => {
                         scores += getScore(subProp.name, subProp.value);
@@ -251,17 +216,130 @@ window.onload = () => {
             // 生成
             parentElement.style.height = 'auto';
             parentElement.append(mainFrameElement);
+        }
+    
+        // 非同期処理を分離
+        async function reDraw() {
+            const isValid = await tryCacheDriverInfoList();
+            if(isValid){
+                draw();
+            }
+        }
+    
+            
+        // 監視を再設定する関数
+        function setObservers() {
+            // キャラ名
+            if (characterInfoElementObserver) {
+                characterInfoElementObserver.disconnect();
+            }
+            characterInfoElementObserver = new MutationObserver(callback);
+            characterInfoElementObserver.observe(characterInfoElement, {
+                childList: false,
+                attributes: true,
+                subtree: false,
+                characterData: false,
+                characterDataOldValue: false,
+                attributeOldValue: false,
+                attributeFilter: ['style']
+            });
+        }
 
+        // 監視のコールバック
+        const callback = (mutationsList, observer) => {
+            for (let mutation of mutationsList) {
+                // キャラ選択
+                if(observer === characterInfoElementObserver 
+                    && mutation.type === 'attributes' && mutation.attributeName === 'style'
+                ){
+                    const oldStyle = mutation.oldValue || ''; 
+                    const newStyle = mutation.target.getAttribute('style') || '';
+                    // backgroundか変更されていればキャラを変更したとみなす
+                    const oldBackground = oldStyle.match(/background:[^;]+/);
+                    const newBackground = newStyle.match(/background:[^;]+/);
+                    if (oldBackground?.[0] !== newBackground?.[0]) {
+                        console.log('キャラが変更された');
+                        reDraw();
+                    }
+                } 
+            }
+        };
 
-            // キャラが変更されたら上記処理を再度行う
+        // 画面を開いてドライバ情報を取得（終わったら戻る）
+        async function tryCacheDriverInfoList(){
+            // bg要素を取得できれば完了
+            const validateEquipInfoElements = (els) => 
+                Array.from(els).some(el => el.querySelector('.bg'));
+            // ステータスにプラスがなく、かつセット効果がないなら装備なしとみなす
+            const stopCondition = () => 
+                !!document.querySelector('.base-add-prop') && 
+                !!document.querySelector('.empty-content');
+            // 上記条件でドライバ情報要素*6を取得
+            const equipInfoElements = await waitForElements(
+                '.equip-info', 
+                validateEquipInfoElements, 
+                undefined, 
+                stopCondition
+            );
+            // 取得できなかったら失敗
+            if(!equipInfoElements){
+                return false;
+            }
+            // ドライバの装備/非装備リスト
+            const equipDriverList = Array.from(equipInfoElements)
+                .map(el => !!el.querySelector('.bg'));
+            const isNothingEquipped = equipDriverList.every(value => value === false);
+            // 装備なしなら終了
+            if(isNothingEquipped){
+                return false;
+            }
+            // 一時的に操作を防ぐ
+            addOverlay();
+            // キャッシュ
+            for (let i = 0; i < equipDriverList.length; i++) {
+                if (equipDriverList[i]) {
+                    equipInfoElements[i].click();
+                    await cacheDriverInfo(i);
+                } else {
+                    driverInfoList[i] = null;
+                }
+            }
+            deleteOverlay();
+            return true;
+        }
+
+        // ユーザーの操作を防ぐ透明なオーバーレイ要素追加
+        function addOverlay(){
+            const overlay = document.createElement("div");
+            overlay.style.position = "fixed";
+            overlay.style.top = "0";
+            overlay.style.left = "0";
+            overlay.style.width = "100vw";
+            overlay.style.height = "100vh";
+            overlay.style.background = "rgba(0, 0, 0, 0)"; 
+            overlay.style.zIndex = "9999";
+            overlay.style.pointerEvents = "none";
+            overlay.id = MY_OVERLAY_ID;
+            document.body.appendChild(overlay);
+        }
+
+        // ユーザーの操作を防ぐ透明なオーバーレイ要素を削除
+        function deleteOverlay(){
+            const overlay = document.querySelector(`#${MY_OVERLAY_ID}`);
+            document.body.removeChild(overlay);
+        }
+
+        // 最初に実行
+        async function setup(){
+            // ドライバ情報取得
+            await tryCacheDriverInfoList();
+            characterInfoElement = await waitForElement('.role-detail-container');
+            // 変更監視開始
+            setObservers();
+
 
             // 自作チェックボックスが変更されたら、計算処理だけ再度行う
 
-
-
-    
-            // 変更監視開始
-            // setObservers();
         }
     
         // スコア要素作成
